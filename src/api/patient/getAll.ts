4b04295f55c49b4f4ef31460591c1f5dbe737e51
Malte Engelhardt <kurvenschubser@googlemail.com>
2024-10-25 13:25:11 +0200

    add basic error handling to api calls

--- src/api/patient/getAll.ts
+++ src/api/patient/getAll.ts
@@ -3,13 +3,20 @@ import type { Patient } from "fhir/r4b";
 
 const getAllPatients = async (): Promise<Patient[]> => {
   const client = FHIR.client({ serverUrl: "https://hapi.fhir.org/baseR4" });
-  return client.request("/Patient", { pageLimit: 1 }).then((response) => {
-    const page = response;
-    if (page.entry.length === 0) {
+  return client
+    .request("/Patient", { pageLimit: 1 })
+    .catch((reason) => {
+      // todo: improve error handling
+      console.error("Failed to fetch patients", reason);
       return [];
-    }
-    return page.entry.map((entry: any) => entry.resource);
-  });
+    })
+    .then((response) => {
+      const page = response;
+      if (page.entry.length === 0) {
+        return [];
+      }
+      return page.entry.map((entry: any) => entry.resource);
+    });
 };
 
 export default getAllPatients;
--- src/api/patient/getOne.ts
+++ src/api/patient/getOne.ts
@@ -3,7 +3,16 @@ import type { Patient } from "fhir/r4b";
 
 const getOnePatient = async (patientId: string): Promise<Patient | null> => {
   const client = FHIR.client({ serverUrl: "https://hapi.fhir.org/baseR4" });
-  return client.request(`/Patient/${patientId}`);
+  return client
+    .request(`/Patient/${patientId}`)
+    .catch((reason) => {
+      // todo: improve error handling
+      console.error("Failed to fetch patient", reason);
+      return null;
+    })
+    .then((response) => {
+      return response;
+    });
 };
 
 export default getOnePatient;
