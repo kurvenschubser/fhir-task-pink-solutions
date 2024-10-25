73a5cb2e470647556049dcaf1f8321923a92b370
Malte Engelhardt <kurvenschubser@googlemail.com>
2024-10-25 13:24:55 +0200

    add nav menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,38 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import PatientList from "./components/patientlist/patientlist";
+import { Link, Outlet } from "react-router-dom";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
 
 const App = () => {
   return (
     <div className="App">
-      <header>
-        <h1 className="text-3xl font-bold underline">
-          FHIR Task Pink Solutions
-        </h1>
-      </header>
+      <Link to={"/"}>
+        <header>
+          <h1 className="text-3xl font-bold underline">
+            FHIR Task Pink Solutions
+          </h1>
+        </header>
+      </Link>
 
-      <PatientList />
+      <div className="min-h-screen bg-background p-8">
+        <div className="mx-auto max-w-2xl">
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuLink>
+                  <Link to={"/patients"}>Patientenliste</Link>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+          <div className="mt-8" />
+          <Outlet />
+        </div>
+      </div>
     </div>
   );
 };
